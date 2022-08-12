36886d0568bc63ffbd8165602e3cd91dfc557840
Aleksandr Pohorelyi <pogorelyaleksandr@gmail.com>
2022-08-13 00:01:14 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -7,15 +7,40 @@ const ingredients = [
   "Condiments",
 ];
 
+// const ingredientsList = document.querySelector("#ingredients");
+// const itemElArray = [];
+
+// ingredients.map((ingredient) => {
+//   const itemEl = document.createElement("li");
+//   itemEl.classList.add("item");
+//   itemEl.textContent = ingredient;
+//   itemElArray.push(itemEl);
+// });
+
+// ingredientsList.append(...itemElArray);
+// console.log(ingredientsList);
+
+
+
+// const ingredientsList = document.querySelector("#ingredients");
+
+// const resultFunction =  ingredients.map((ingredient) => {
+//   const itemEl = document.createElement("li");
+//   itemEl.classList.add("item");
+//   itemEl.textContent = ingredient;
+// return itemEl
+// });
+
+// ingredientsList.append(...resultFunction);
+// console.log(ingredientsList);
+
+
 const ingredientsList = document.querySelector("#ingredients");
-const itemElArray = [];
 
-ingredients.map((ingredient) => {
-  const itemEl = document.createElement("li");
-  itemEl.classList.add("item");
-  itemEl.textContent = ingredient;
-  itemElArray.push(itemEl);
-});
+const result = ingredients.map((ingredient) => {
+  const itemEl = `<li class = "item">${ingredient}</li>`;
+  return itemEl
+}).join("");
 
-ingredientsList.append(...itemElArray);
+ingredientsList.insertAdjacentHTML("afterbegin",result);
 console.log(ingredientsList);
